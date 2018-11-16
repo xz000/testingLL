66bdf49accb6dfb9ee27cd6c9265bd13edab6b14
xz000 <xz000@users.noreply.github.com>
2018-11-16 16:52:46 +0800

    1

---
+++ Assets/Scripts/HostCook.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HostCook : MonoBehaviour {
+    public Text HostNOtext;
+    
+    public int HostStr2int()
+    {
+        int ttt;
+        int.TryParse(HostNOtext.text, out ttt);
+        return ttt;
+    }
+}
---
+++ Assets/Scripts/HostToggle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class HostToggle : MonoBehaviour {
+    public Toggle HT;
+    public InputField Hinput;
+    public Text Htext;
+    public Text SLabel;
+    public int HostNO;
+    public int HChannelID;
+    ConnectionConfig CCFIG;
+    HostTopology HOSTTT;
+
+    public void ClickHT()
+    {
+        if (HT.isOn)
+            Checkstart();
+        else
+            Hstop();
+    }
+
+    void Checkstart()
+    {
+        Hinput.interactable = false;
+        int.TryParse(Htext.text, out HostNO);
+        if (HostNO <= 0)
+        {
+            HT.isOn = false;
+            return;
+        }
+        Hstart();
+    }
+
+    void Hstart()
+    {
+        NetworkTransport.Init();
+        CCFIG = new ConnectionConfig();
+        HChannelID = CCFIG.AddChannel(QosType.Reliable);
+        HOSTTT = new HostTopology(CCFIG, 10);
+        NetworkTransport.AddHost(HOSTTT, HostNO);
+        SLabel.text = "Host on :" + HostNO.ToString();
+    }
+
+    void Hstop()
+    {
+        Hinput.interactable = true;
+        NetworkTransport.Shutdown();
+        SLabel.text = "Host Stoped";
+    }
+}
