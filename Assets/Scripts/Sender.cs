d528de47b736c90fe486cd917a2d58530a0abc2c
xz000 <xz000@users.noreply.github.com>
2018-11-19 16:02:48 +0800

    20181119w

---
+++ Assets/Scripts/Sender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Sender : MonoBehaviour {
+
+    public byte[] buffer;
+    public int sz;
+    public byte error;
+    public bool isServer;
+
+    public void sendhi(int hstid, int cnid, int chanid)
+    {
+        NetworkTransport.Send(hstid, cnid, chanid, buffer, sz, out error);
+    }
+}
