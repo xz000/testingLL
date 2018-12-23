4552fe6ba1019c71aef685adf228c08c189b108e
xvzan <xvzan0@gmail.com>
2018-12-23 15:11:18 +0800

    20181223-2

---
+++ Assets/Scripts/Play/ShowMyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowMyInfo : MonoBehaviour
+{
+    public GameObject MyInfoText;
+    GameObject MyInfo;
+    Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        Canvas TheCanvas = GameObject.FindObjectOfType<Canvas>();
+        MyInfo = GameObject.Instantiate(MyInfoText, TheCanvas.gameObject.transform);
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MyInfo.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.down * 0.8f);
+        MyInfo.GetComponent<Text>().text = rb2d.position.x + "," + rb2d.position.y;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(MyInfo);
+    }
+}
