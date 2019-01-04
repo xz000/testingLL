b03fcc87fe7854ef4df4d348c8c324acdd6032a5
xz000 <xz000@users.noreply.github.com>
2019-01-04 13:52:48 +0800

    20190104-1B

---
+++ Assets/Scripts/CatcherKeys.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatcherKeys : MonoBehaviour
+{
+    public SkillCode? CodeG;
+    public SkillCode? CodeY;
+    public SkillCode? CodeT;
+    public SkillCode? CodeR;
+    public SkillCode? CodeE;
+    public SkillCode? CodeD;
+    public SkillCode? CodeC;
+    public SkillCode? CodeF;
+}
