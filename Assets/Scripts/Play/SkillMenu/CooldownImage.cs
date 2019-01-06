72a5aebb26362d7611d8b91691095819e1722241
xvzan <xvzan0@gmail.com>
2019-01-06 22:03:53 +0800

    20190106-2a

--- Assets/Scripts/Play/SkillMenu/CooldownImage.cs
+++ Assets/Scripts/Play/SkillMenu/CooldownImage.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CooldownImage : MonoBehaviour {
-
-    public float IconFillAmount = 1;
+public class CooldownImage : MonoBehaviour
+{
     public Image Icon;
     public delegate float delef();
     public delef Fif;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         Icon = gameObject.GetComponent<Image>();
         enabled = false;
-	}
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         Icon.fillAmount = Fif();
     }
 }
