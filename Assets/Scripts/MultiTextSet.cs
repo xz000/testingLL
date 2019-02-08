d05b08bbdffb84b5cf9502d4ebc8bd652dd1ecdf
xvzan <xvzan0@gmail.com>
2019-02-08 20:53:18 +0800

    20190208

---
+++ Assets/Scripts/MultiTextSet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiTextSet : MonoBehaviour
+{
+    public string TextEn;
+    public string TextCn;
+
+    private void OnEnable()
+    {
+        switch (LanguageSet.CurrentLanguage)
+        {
+            case "schinese":
+                GetComponent<Text>().text = TextCn;
+                break;
+        }
+    }
+}
