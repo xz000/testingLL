07f5064279e354b39cc1761b74a47a7f0676227f
xvzan <xvzan0@gmail.com>
2019-02-08 20:53:18 +0800

    20190208

---
+++ Assets/Scripts/LanguageSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class LanguageSet : MonoBehaviour
+{
+    public static string CurrentLanguage;
+    public GameObject CV2;
+
+    void Start()
+    {
+        SteamAPI.Init();
+        CurrentLanguage = SteamApps.GetCurrentGameLanguage();
+        CV2.SetActive(true);
+    }
+}
