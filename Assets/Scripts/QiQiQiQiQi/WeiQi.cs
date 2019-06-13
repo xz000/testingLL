33a92beab4f63550895510650d6332a6b84a43a4
xvzan <xvzan0@gmail.com>
2019-06-13 20:26:15 +0800

    QiZi Alpha

---
+++ Assets/Scripts/QiQiQiQiQi/WeiQi.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeiQi : MonoBehaviour
+{
+    public GameObject qizi;
+    public static Color PanColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PanColor = GetComponent<SpriteRenderer>().color;
+        chushihuaQi();
+    }
+
+    void chushihuaQi()
+    {
+
+        for (int i = -9; i < 10; i++)
+        {
+            for (int j = -9; j < 10; j++)
+            {
+                GameObject nq = GameObject.Instantiate(qizi, new Vector3(i, j, 0), Quaternion.identity);
+                nq.GetComponent<WeiQiZi>().setXY(i, j);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/QiQiQiQiQi/WeiQiZi.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeiQiZi : MonoBehaviour
+{
+    public SpriteRenderer srDa;
+    public SpriteRenderer srZhong;
+    public SpriteRenderer srXiao;
+    public int x;
+    public int y;
+    public void Start()
+    {
+        srDa.color = srZhong.color = srXiao.color = WeiQi.PanColor;
+    }
+
+    public void setXY(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+
+    public void goWhite()
+    {
+        srZhong.color = Color.white;
+    }
+    
+    public void goBlack()
+    {
+        srZhong.color = Color.black;
+    }
+
+    public void goDie()
+    {
+        srZhong.color = WeiQi.PanColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        srDa.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        srDa.color = WeiQi.PanColor;
+    }
+}
