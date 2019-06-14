815daaf11d2690418d13c2c06f1fb9e547954e64
xvzan <xvzan0@gmail.com>
2019-06-14 21:23:56 +0800

    Xia AAA

--- Assets/Scripts/QiQiQiQiQi/WeiQi.cs
+++ Assets/Scripts/QiQiQiQiQi/WeiQi.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WeiQi : MonoBehaviour
 {
+    bool nowBlack = false;
     public GameObject qizi;
     public static Color PanColor;
     // Start is called before the first frame update
@@ -11,6 +12,12 @@ public class WeiQi : MonoBehaviour
     {
         PanColor = GetComponent<SpriteRenderer>().color;
         chushihuaQi();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            CatchHim();
     }
 
     void chushihuaQi()
@@ -23,6 +30,45 @@ public class WeiQi : MonoBehaviour
                 GameObject nq = GameObject.Instantiate(qizi, new Vector3(i, j, 0), Quaternion.identity);
                 nq.GetComponent<WeiQiZi>().setXY(i, j);
             }
+        }
+    }
+
+    public void ChangeColor()
+    {
+        nowBlack = !nowBlack;
+    }
+
+    public void ToBlack()
+    {
+        nowBlack = true;
+    }
+
+    public void ToWhite()
+    {
+        nowBlack = false;
+    }
+
+    public void eat1B()
+    {
+
+    }
+
+    public void eat1W()
+    {
+
+    }
+
+    void CatchHim()
+    {
+        Collider2D hit = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        if (!hit)
+            return;
+        if (hit.GetComponent<WeiQiZi>())
+        {
+            if (nowBlack)
+                hit.GetComponent<WeiQiZi>().goBlack();
+            else
+                hit.GetComponent<WeiQiZi>().goWhite();
         }
     }
 }
