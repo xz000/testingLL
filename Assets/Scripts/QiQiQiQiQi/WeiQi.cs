2ae04453977c18786cb5be0f09f4e3578521a2b0
xvzan <xvzan0@gmail.com>
2019-06-15 19:22:32 +0800

    C Alpha

--- Assets/Scripts/QiQiQiQiQi/WeiQi.cs
+++ Assets/Scripts/QiQiQiQiQi/WeiQi.cs
@@ -8,6 +8,8 @@ public class WeiQi : MonoBehaviour
     private int[,] Lon = new int[19, 19];
     bool nowBlack = false;
     public GameObject qizi;
+    public WeiQiControl blackEaten;
+    public WeiQiControl whiteEaten;
     public static Color PanColor;
     // Start is called before the first frame update
     void Start()
@@ -41,6 +43,9 @@ public class WeiQi : MonoBehaviour
 
     private void jieLon()
     {
+        int color = 1;
+        if (nowBlack)
+            color = 2;
         Lon = new int[19, 19];
         int n = 0;
         for (int i = 0; i < 19; i++)
@@ -49,28 +54,75 @@ public class WeiQi : MonoBehaviour
             {
                 if (Lon[i, j] != 0)
                     continue;
-                if (Chou[i, j] != 0)
+                if (Chou[i, j] == color)
                 {
                     n++;
-                    lianZhu(i, j, n);
+                    if (lianZhu(i, j, n))
+                        killNum(n);
                 }
             }
         }
     }
 
-    private void lianZhu(int i, int j, int n)
+    private void killNum(int n)
     {
+        for (int i = 0; i < 19; i++)
+        {
+            for (int j = 0; j < 19; j++)
+            {
+                if (Lon[i, j] == n)
+                    killSingle(i, j);
+            }
+        }
+    }
+
+    private void killSingle(int i, int j)
+    {
+        switch (Chou[i, j])
+        {
+            case 1:
+                eat1B();
+                break;
+            case 2:
+                eat1W();
+                break;
+        }
+        Chou[i, j] = 0;
+        Collider2D hit = Physics2D.OverlapPoint(new Vector2(i - 9, j - 9));
+        hit.GetComponent<WeiQiZi>().goDie();
+    }
+
+    private bool lianZhu(int i, int j, int n)
+    {
+        bool sha = true;
         Lon[i, j] = n;
-        Debug.Log(i + "," + j + "," + n);
+        //Debug.Log(i + "," + j + "," + n);
         for (int a = -1; a < 2; a += 2)
         {
             if (19 > i + a && i + a >= 0)
-                if (Chou[i + a, j] == Chou[i, j] && Lon[i + a, j] == 0)
-                    lianZhu(i + a, j, n);
+                switch (Chou[i + a, j])
+                {
+                    case 0:
+                        sha = false;
+                        break;
+                    default:
+                        if (Chou[i + a, j] == Chou[i, j] && Lon[i + a, j] == 0)
+                            sha = sha & lianZhu(i + a, j, n);
+                        break;
+                }
             if (19 > j + a && j + a >= 0)
-                if (Chou[i, j + a] == Chou[i, j] && Lon[i, j + a] == 0)
-                    lianZhu(i, j + a, n);
+                switch (Chou[i, j + a])
+                {
+                    case 0:
+                        sha = false;
+                        break;
+                    default:
+                        if (Chou[i, j + a] == Chou[i, j] && Lon[i, j + a] == 0)
+                            sha = sha & lianZhu(i, j + a, n);
+                        break;
+                }
         }
+        return sha;
     }
 
     void chushihuaQi()
@@ -124,15 +176,12 @@ public class WeiQi : MonoBehaviour
             else
                 setChou(hit.GetComponent<WeiQiZi>(), 2);
             jieLon();
+            ChangeColor();
+            jieLon();
         }
         if (hit.GetComponent<WeiQiControl>())
         {
             hit.GetComponent<WeiQiControl>().pickMe();
         }
-    }
-
-    void Panduan()
-    {
-
     }
 }
