1eb7213be4b002b3480ea304e0f971f37b57e310
xvzan <xvzan0@gmail.com>
2019-06-16 21:54:11 +0800

    S Alpha

---
+++ Assets/Scripts/QiQiQiQiQi/WeiQiScore.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeiQiScore : MonoBehaviour
+{
+    public bool IBlack;
+    public Color HeColor;
+    public SpriteRenderer srDa;
+    public SpriteRenderer srZhong;
+    public SpriteRenderer srXiao;
+    public WeiQi qipan;
+    public int meScore = 0;
+    public GameObject MyInfoText;
+    GameObject MyInfo;
+
+    // Use this for initialization
+    void Start()
+    {
+        srDa.color = HeColor;
+        GameObject TheCanvas = GameObject.Find("Canvas");
+        MyInfo = GameObject.Instantiate(MyInfoText, TheCanvas.transform);
+        MyInfo.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+        MyInfo.GetComponent<Text>().text = meScore.ToString();
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(MyInfo);
+    }
+
+    private void OnMouseEnter()
+    {
+        srDa.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        srDa.color = HeColor;
+    }
+
+    public void mePlusone()
+    {
+        meScore++;
+        MyInfo.GetComponent<Text>().text = meScore.ToString();
+    }
+}
