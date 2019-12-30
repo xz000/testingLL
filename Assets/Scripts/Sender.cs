f1c1045e81fa9e59a06bfe78317d5cee891bf00b
Zan Xv <xvzan0@gmail.com>
2019-12-31 00:43:05 +0800

    Round New Beta

--- Assets/Scripts/Sender.cs
+++ Assets/Scripts/Sender.cs
@@ -81,7 +81,6 @@ public class Sender : MonoBehaviour
         {
             SendHello(5);
             Cntb = new bool[Cntb.Length];
-            Learning = false;
             Debug.Log("Local Skill Level Temp Set,Hello5 Sent");
         }
     }
@@ -105,7 +104,7 @@ public class Sender : MonoBehaviour
         {
             Debug.Log("Connect Do");
             started = true;
-            //Learning = false;
+            Learning = false;
             SignalLight.color = Color.green;
             MyNS.meEnable();//开启netwriter
             //MyNS.enabled = true;
