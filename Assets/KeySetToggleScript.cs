4dc937b599ab08df0862e85d87c4ad342926dec9
Zan Xv <xvzan0@gmail.com>
2020-01-11 19:28:33 +0800

    按键设置 Alpha

---
+++ Assets/KeySetToggleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeySetToggleScript : MonoBehaviour
+{
+    public GameObject SetPanel;
+    public GetKeyScript[] getKeyScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Toggle>().onValueChanged.AddListener(ClickSwitch);
+    }
+
+    void ClickSwitch(bool a)
+    {
+        SetPanel.SetActive(a);
+    }
+
+    public void FindExistAndHandle(string existed, string newname)
+    {
+        foreach (GetKeyScript script in getKeyScripts)
+        {
+            if (script.GetName() == existed)
+            {
+                script.GetComponent<SetKeyScript>().SetPref(newname);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Play/TestMenu/SetKeyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GetKeyScript))]
+[RequireComponent(typeof(Toggle))]
+public class SetKeyScript : MonoBehaviour
+{
+    public KeySetToggleScript keySetToggleScript;
+    public void SetPref(string s)
+    {
+        PlayerPrefs.SetString(GetComponent<GetKeyScript>().MyPrefName, s);
+        GetComponent<GetKeyScript>().SetText();
+    }
+
+    public void SelfEnable(bool a)
+    {
+        enabled = a;
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            enabled = false;
+            GetComponent<Toggle>().isOn = false;
+            keySetToggleScript.FindExistAndHandle(e.keyCode.ToString(), GetComponent<GetKeyScript>().GetName());
+            SetPref(e.keyCode.ToString());
+            Debug.Log("Key Pressed");
+        }
+    }
+}
